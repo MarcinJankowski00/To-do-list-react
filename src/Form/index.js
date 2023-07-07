3e7ca247955642e471bfadd34394efe98080568f
Marcin Jankowski <marcin5771@gmail.com>
2023-07-07 10:43:05 +0200

    focus after submit added

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,18 +1,24 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { Sheet, Input, Button } from "./styled";
 
 const Form = ({ addNewTask }) => {
     const [newTaskContent, setNewTaskContent] = useState("");
-    
+    const inputRef = useRef(null);
+
     const onFormSubmit = (event) => {
         event.preventDefault();
         addNewTask(newTaskContent.trim());
         setNewTaskContent("");
     };
 
+    const focusInput = () => {
+        inputRef.current.focus();
+    };
+
     return (
         <Sheet onSubmit={onFormSubmit}>
             <Input
+                ref={inputRef}
                 value={newTaskContent}
                 placeholder="Co jest do zrobienia?"
                 required
@@ -20,7 +26,7 @@ const Form = ({ addNewTask }) => {
                 id="input"
                 onChange={(event) => setNewTaskContent(event.target.value)}
             />
-            <Button>Dodaj zadanie</Button>
+            <Button onClick={focusInput}>Dodaj zadanie</Button>
         </Sheet>
     );
 };
