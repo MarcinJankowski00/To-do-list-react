66e7f67dea2274cdd868fe3b8d39643022722c52
Marcin Jankowski <marcin5771@gmail.com>
2024-08-04 15:37:50 +0200

    Text about the author added

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Section from '../../common/Section';
 import Container from '../../common/Container';
+import { Text } from './styled'
 
 function AuthorPage() {
     return (
@@ -11,9 +12,12 @@ function AuthorPage() {
             <Section
                 title="Marcin Jankowski"
                 content={
-                    <a>
-                        nanana
-                    </a>
+                    <Text>
+                        Ukończyłem szkołę podstawową i gimnazjum w Knyszynie. Następnie ukończyłem liceum w Białymstoku na profilu mat-fiz-inf.
+                        Aktualnie studiuje Ekonomie na Uniwersytecie w Białymstoku, oraz uczęstnicze w kursie Frontend developer.
+                        Moimi pierwszym hobby jest siłownia. Ćwiczę parę razy w tygodniu w domowej siłowni, w piwnicy. 
+                        Kolejnym hobby jest bieganie, biegam prawie codziennie. W wolnym czasie lubię pograć w gry, obejrzeć dobry film lub serial.
+                    </Text>
                 }
             />
         </Container>
---
+++ src/features/author/styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Text =styled.p`
+    text-align: justify;
+    margin: 0;
+`
