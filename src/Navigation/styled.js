4ffe472c12042923e73221f4fb71c407702cb967
Marcin Jankowski <marcin5771@gmail.com>
2024-08-13 17:28:36 +0200

    Navigation is in center now and button hover style added

--- src/Navigation/styled.js
+++ src/Navigation/styled.js
@@ -13,14 +13,23 @@ export const List = styled.ul`
     display: flex;
     justify-content: center;
     list-style: none;
+    padding-left: 0;
 `;
 
 export const StyledNavLink = styled(NavLink)`
-    &.active {
-        font-weight: bold;
-    }
-    padding: 25px;
+    margin: 25px;
     text-decoration: none;
     outline: none;
     color: ${({ theme }) => theme.color.white};
+
+    &:hover {
+        filter: brightness(120%);
+        padding-bottom: 4px;
+        border-bottom: 1px solid ${({ theme }) => theme.color.white};
+        outline: none;
+    }
+
+    &.active {
+        font-weight: bold;
+    }
 `;
