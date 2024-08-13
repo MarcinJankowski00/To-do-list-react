f4e7c24a5ae49b76f256072916cec2c62911d408
Marcin Jankowski <marcin5771@gmail.com>
2024-08-13 17:27:04 +0200

    BrowserRouter changed to HashRouter

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from "react";
 import TaskPage from "./features/tasks/TaskPage"
 import TasksPage from "./features/tasks/TasksPage"
 import AuthorPage from "./features/author/AuthorPage"
-import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
+import { HashRouter, Redirect, Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 import { toAuthor, toTask, toTasks } from "./routes";
 import Navigation from "./Navigation";
 
 const App = () => (
-    <BrowserRouter basename="/To-do-list-react">
+    <HashRouter basename="/To-do-list-react">
         <Navigation />
         <Switch>
             <Route path={toTask()}>
@@ -23,7 +23,7 @@ const App = () => (
                 <Redirect to={toTasks()} />
             </Route>
         </Switch>
-    </BrowserRouter>
+    </HashRouter>
 );
 
 export default App;
